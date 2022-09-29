508bb14b3038fe01cc6ab88c6be4edf2a6b725ce
RikVandermullen <rik.vandermullen@gmail.com>
2022-09-29 16:56:53 +0200

    Add project files.

---
+++ SSWF_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SSWF_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
