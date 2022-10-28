3c484bf0279924038282ad7c898d0011e3e8ba7e
RikVandermullen <rik.vandermullen@gmail.com>
2022-10-28 12:26:55 +0200

    Changed Connection Strings

---
+++ EF/Migrations/20221028102349_Updated online database.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TGTG_EF.Migrations
+{
+    public partial class Updatedonlinedatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- SSWF_Project/Program.cs
+++ SSWF_Project/Program.cs
@@ -47,11 +47,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 
-    //await SeedDatabase();
+    await SeedDatabase();
 }
 else
 {
-    //await SeedDatabase();
+    await SeedDatabase();
     // Only in dev env seed with dummy data -->
 
 }
